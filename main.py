3358562ccbd692b2c54a038b469eb023d324e07b
100472037@alumnos.uc3m.es <Simpaticas_22>
2023-11-28 08:31:37 +0100

    Co-authored-by: 100471948 <celiiaaa@users.noreply.github.com>

--- main.py
+++ main.py
@@ -11,104 +11,24 @@ import getpass
 import random
 from claves import Claves
 import json
+from tkinter import messagebox
 import sys
 
-
-def Registro():
-    """
-    Función que registra a un usuario en la aplicación
-    """
-    ventana_registro = tkinter.Tk()
-    ventana_registro.geometry("550x650")
-    ventana_registro.resizable(False, False)
-    ventana_registro.config(bg='#ADAFE1')
-    titulo = tkinter.Label(ventana_registro, text = "Registro", font=("Rockwell Nova Extra Bold", 25), fg= '#2b0d48')  
-    titulo.pack(fill = tkinter.X, pady = 20)
-    ventana_registro.title("Registro")
-    label_nombre = tkinter.Label(ventana_registro, text = "Nombre completo: ", font=("Rockwell Nova Bold", 20), fg= '#2b0d48',  bg='#ADAFE1') 
-    entrada = Entry(ventana_registro) 
-    validaciones = ValidarCampos()
-    label_nombre.pack(fill = tkinter.BOTH, pady = 10)
-    entrada.pack(fill = tkinter.BOTH, pady = 10)
-
-    nombre = input("Introducir nombre completo: ")
-    correo = input("Introducir correo: ")
-    res_correo = validaciones.ComprobarCorreo(correo)
-
-    #para cada campo se comprueba que sea válido. Si no lo es, se vuelve a pedir
-    while not res_correo:                                       
-        print("El correo no es valido")
-        correo = input("Vuelve a introducir tu correo: ")
-        res_correo = validaciones.ComprobarCorreo(correo)
-    
-    telefono = input("Introducir telefono: ")
-    res_telefono = validaciones.ComprobarTelefono(telefono)
-    
-    while not res_telefono:
-        print("El teléfono no es valido")
-        telefono = input("Vuelve a introducir tu teléfono: ")
-        res_telefono= validaciones.ComprobarTelefono(telefono)
-    
-    contrasenia = getpass.getpass("Introducir contraseña: ")            
-    res_contrasenia = validaciones.ComprobarConstrasenia(contrasenia)
-    validaciones.ValidarContrasenia(res_contrasenia)
-    contrasenia_2 = getpass.getpass("Repite la contraseña: ")
-
-    #La contraseña se pide dos veces y se comprueba que coincide
-    while contrasenia != contrasenia_2:                                           
-        print("Las contraseñas no coinciden")
-        contrasenia = getpass.getpass("Vuelve a introducir contraseña: ")
-        res_contrasenia = validaciones.ComprobarConstrasenia(contrasenia)
-        validaciones.ValidarContrasenia(res_contrasenia)
-        contrasenia_2 = getpass.getpass("Repite la contraseña: ")
-
-    # Guardar la contraseña derivada en una base de datos con el correo y el salt
-    salt = os.urandom(16)
-    # Se deriva la contraseña
-    kdf = Scrypt(
-        salt=salt,
-        length=32,
-        n=2**14,
-        r=8,
-        p=1,
-    )
-    key = kdf.derive(bytes(contrasenia.encode("utf-8")))
-
-    bd = BaseDeUsuarios()
-    #Si el usuario ya estaba registrado se le redirige al inico de sesión
-    if bd.find_data(correo) is not None:
-        print("El usuario ya existe. Inicia sesion con tu cuenta")
-        print("----------------------Incio de sesion----------------------")
-        nombre, correo = InicioSesion()
-        return nombre, correo
-
-   
-   
-    #se crea una clave privada para el usuario y se almacena de forma segura. La clave pública se guarda en una base de datos
-    print("Adios")
-    path = os.path.dirname(__file__) + "/usuarios/" + correo
-    os.makedirs(path, exist_ok=True)
-    print("hola")
-    Claves(path, correo, telefono).CrearClavePrivada()
-    #se guarda el usuario en la base de datos
-    nuevo_usuario={"Nombre": nombre, "Correo": correo, "Telefono": telefono, "Contrasenia_derivada": key.decode('latin-1'), "Salt": salt.decode('latin-1')}
-    bd.add_item(nuevo_usuario)
-    ventana_registro.mainloop()
-    return nombre, correo
-
-def InicioSesion():
-    """
-    Función para iniciar sesión
-    """
-    correo = input("Introducir correo: ")
-    contrasenia = getpass.getpass("Introducir contraseña: ")
+def ComprobarConstraseña(correo, contrasenia):
     bd = BaseDeUsuarios()
     usuario = bd.find_data(correo)
-    if usuario is None:                                                                   #Si el usuario no está registrado se le redirige al registro
-        print("El usuario no existe. Regístrate para poder usar la aplicación")
-        print("----------------------Registro de usuarios----------------------")
-        
-        return Registro()
+    ventana_error = tkinter.Tk()
+    ventana_error.title("Error")
+    ventana_error.geometry("300x150")
+    ventana_error.config(bg='#ADAFE1')
+    ventana_error.resizable(False, False)
+    if usuario is None:  
+        error = tkinter.Label(ventana_error, text = "El usuario no está registrado", font=("Rockwell Nova Bold", 12), fg= '#2b0d48',  bg='white')
+        error.pack(fill = tkinter.BOTH, pady = 20) 
+        button_registro = Button(ventana_error, text="Ir al registro", command= Registro, font=("Segoe UI", 10))
+        button_registro.pack(pady= 10)
+        ventana_error.mainloop()                                                             #Si el usuario no está registrado se le redirige al registro
+        return
     
     salt = usuario["Salt"]
     key = usuario["Contrasenia_derivada"]
@@ -127,12 +47,15 @@ def InicioSesion():
             kdf.verify(bytes(contrasenia.encode("latin-1")), key.encode('latin-1'))
             break
         except:                                                                              
-            print("Contraseña incorrecta")
-            contrasenia = getpass.getpass("Vuelve a introducir contraseña: ")
+            error = tkinter.Label(ventana_error, text = "Contraseña incorrecta", font=("Rockwell Nova Bold", 12), fg= '#2b0d48',  bg='white')
+            error.pack(fill = tkinter.BOTH, pady = 20) 
+            button_aceptar = Button(ventana_error, text="Aceptar", command= ventana_error.destroy, font=("Segoe UI", 10))
+            button_aceptar.pack(pady= 10)
+            print(i)
+            #ventana_error.mainloop()
             if i == 1:
                 raise Excepcion("Contraseña incorrecta. Has superado el numero de intentos")  
 
-    print("Inicio de sesion correcto")
     #El 50% de las veces que se inicia sesión se cambia el token de la contraseña
     cambio_token = random.randint(1, 2)
     if cambio_token == 1:
@@ -152,18 +75,166 @@ def InicioSesion():
 
         bd.save_store()
 
-    return usuario["Nombre"], usuario["Correo"]
+
+def Validar_campos(nombre, correo, telefono, contrasenia, contrasenia2):
+    validaciones = ValidarCampos()
+    ventana_error = tkinter.Tk()
+    ventana_error.title("Error")
+    ventana_error.geometry("300x150")
+    ventana_error.config(bg='#ADAFE1')
+    ventana_error.resizable(False, False)
+    if not nombre:
+        error = tkinter.Label(ventana_error, text = "El nombre no puede estar vacío", font=("Rockwell Nova Bold", 12), fg= '#2b0d48',  bg='white')
+        error.pack(fill = tkinter.BOTH, pady = 20)
+        button_aceptar = Button(ventana_error, text="Aceptar", command= ventana_error.destroy, font=("Segoe UI", 10))
+        button_aceptar.pack(pady= 10)
+        ventana_error.mainloop() 
+        return
+    res_correo = validaciones.ComprobarCorreo(correo)
+    if not res_correo:
+        error = tkinter.Label(ventana_error, text = "El correo no es valido", font=("Rockwell Nova Bold", 12), fg= '#2b0d48',  bg='white')
+        error.pack(fill = tkinter.BOTH, pady = 20)
+        button_aceptar = Button(ventana_error, text="Aceptar", command= ventana_error.destroy, font=("Segoe UI", 10))
+        button_aceptar.pack(pady= 10)
+        ventana_error.mainloop()
+        return
+    res_telefono = validaciones.ComprobarTelefono(telefono)
+    if not res_telefono:
+        error = tkinter.Label(ventana_error, text = "El teléfono no es valido", font=("Rockwell Nova Bold", 12), fg= '#2b0d48',  bg='white')
+        error.pack(fill = tkinter.BOTH, pady = 20)
+        button_aceptar = Button(ventana_error, text="Aceptar", command= ventana_error.destroy, font=("Segoe UI", 10))
+        button_aceptar.pack(pady= 10)
+        ventana_error.mainloop()
+        return
+    res_contrasenia = validaciones.ComprobarConstrasenia(contrasenia)
+    if not res_contrasenia:
+        error = tkinter.Label(ventana_error, text = "La contraseña no es valida", font=("Rockwell Nova Bold", 12), fg= '#2b0d48',  bg='white')
+        error.pack(fill = tkinter.BOTH, pady = 20)
+        button_aceptar = Button(ventana_error, text="Aceptar", command= ventana_error.destroy, font=("Segoe UI", 10))
+        button_aceptar.pack(pady= 10)
+        ventana_error.mainloop()
+        return
+    if contrasenia != contrasenia2:
+        error = tkinter.Label(ventana_error, text = "Las contraseñas no coinciden", font=("Rockwell Nova Bold", 12), fg= '#2b0d48',  bg='white')
+        error.pack(fill = tkinter.BOTH, pady = 20)
+        button_aceptar = Button(ventana_error, text="Aceptar", command= ventana_error.destroy, font=("Segoe UI", 10))
+        button_aceptar.pack(pady= 10)
+        ventana_error.mainloop()   
+        return
+    ventana_error.destroy()
+    bd = BaseDeUsuarios()
+    #Si el usuario ya estaba registrado se le redirige al inico de sesión
+    if bd.find_data(correo) is not None:
+        print("El usuario ya existe. Inicia sesion con tu cuenta")
+        print("----------------------Incio de sesion----------------------")
+        nombre, correo = InicioSesion()
+        return nombre, correo
+
+    # Guardar la contraseña derivada en una base de datos con el correo y el salt
+    salt = os.urandom(16)
+    # Se deriva la contraseña
+    kdf = Scrypt(
+        salt=salt,
+        length=32,
+        n=2**14,
+        r=8,
+        p=1,
+    )
+    key = kdf.derive(bytes(contrasenia.encode("utf-8")))
+
+    #se crea una clave privada para el usuario y se almacena de forma segura. La clave pública se guarda en una base de datos
+    path = os.path.dirname(__file__) + "/usuarios/" + correo
+    os.makedirs(path, exist_ok=True)
+    Claves(path, correo, telefono).CrearClavePrivada()
+    #se guarda el usuario en la base de datos
+    nuevo_usuario={"Nombre": nombre, "Correo": correo, "Telefono": telefono, "Contrasenia_derivada": key.decode('latin-1'), "Salt": salt.decode('latin-1')}
+    bd.add_item(nuevo_usuario)
+    return
+
+def Registro():
+    """
+    Función que registra a un usuario en la aplicación
+    """        
+    ventana_registro = tkinter.Tk()
+    ventana_registro.geometry("550x650+50+50")
+    ventana_registro.resizable(False, False)
+    ventana_registro.config(bg='#ADAFE1')
+    titulo = tkinter.Label(ventana_registro, text = "Registro", font=("Rockwell Nova Extra Bold", 25), fg= '#2b0d48')  
+    titulo.pack(fill = tkinter.X, pady = 20)
+    ventana_registro.title("Registro")
+    
+    label_nombre = tkinter.Label(ventana_registro, text = "Nombre completo: ", font=("Rockwell Nova Bold", 15), fg= '#2b0d48',  bg='#ADAFE1') 
+    label_correo = tkinter.Label(ventana_registro, text = "Correo: ", font=("Rockwell Nova Bold", 15), fg= '#2b0d48',  bg='#ADAFE1') 
+    label_telefono = tkinter.Label(ventana_registro, text = "Telefono: ", font=("Rockwell Nova Bold", 15), fg= '#2b0d48',  bg='#ADAFE1') 
+    label_contrasenia1 = tkinter.Label(ventana_registro, text = "Contraseña: ", font=("Rockwell Nova Bold", 15), fg= '#2b0d48',  bg='#ADAFE1') 
+    label_contrasenia2 = tkinter.Label(ventana_registro, text = "Repite la contraseña: ", font=("Rockwell Nova Bold", 15), fg= '#2b0d48',  bg='#ADAFE1') 
+    
+    entrada_nombre = Entry(ventana_registro)
+    entrada_correo = Entry(ventana_registro)
+    entrada_telefono = Entry(ventana_registro) 
+    entrada_contrasenia1 = Entry(ventana_registro, show="*") 
+    entrada_contrasenia2 = Entry(ventana_registro, show="*") 
+    
+    label_nombre.pack(fill = tkinter.BOTH, pady = 3)
+    entrada_nombre.pack(fill = tkinter.BOTH, pady = 10, padx= 70, ipady= 5)
+
+    label_correo.pack(fill = tkinter.BOTH, pady = 3)
+    entrada_correo.pack(fill = tkinter.BOTH, pady = 10, padx= 70, ipady= 5)
+
+    label_telefono.pack(fill = tkinter.BOTH, pady =3)
+    entrada_telefono.pack(fill = tkinter.BOTH, pady = 10, padx= 70, ipady= 5)
+
+    label_contrasenia1.pack(fill = tkinter.BOTH, pady = 3)
+    entrada_contrasenia1.pack(fill = tkinter.BOTH, pady = 10, padx= 70, ipady= 5)
+
+    label_contrasenia2.pack(fill = tkinter.BOTH, pady = 3)
+    entrada_contrasenia2.pack(fill = tkinter.BOTH, pady = 10, padx= 70, ipady= 5)
+    
+    button_aceptar = Button(ventana_registro, text="Aceptar", command= lambda: Validar_campos(entrada_nombre.get(), entrada_correo.get(), entrada_telefono.get(), entrada_contrasenia1.get(), entrada_contrasenia2.get()), font=("Segoe UI", 10))
+    button_volver = Button(ventana_registro, text="Volver", command= ventana_registro.destroy, font=("Segoe UI", 10))
+    button_aceptar.pack(side="left", pady= 10, padx= 150)
+    button_volver.pack(side="left",pady = 10)
+    ventana_registro.mainloop()
+    return #nombre, correo
+
+def InicioSesion():
+    """
+    Función para iniciar sesión
+    """
+    ventana_inicio = tkinter.Tk()
+    ventana_inicio.geometry("550x650+50+50")
+    ventana_inicio.resizable(False, False)
+    ventana_inicio.config(bg='#ADAFE1')
+    titulo = tkinter.Label(ventana_inicio, text = "Incio de sesión", font=("Rockwell Nova Extra Bold", 25), fg= '#2b0d48')  
+    titulo.pack(fill = tkinter.X, pady = 20)
+    ventana_inicio.title("Inicio")
+
+    label_correo = tkinter.Label(ventana_inicio, text = "Correo: ", font=("Rockwell Nova Bold", 15), fg= '#2b0d48',  bg='#ADAFE1') 
+    label_contrasenia1 = tkinter.Label(ventana_inicio, text = "Contraseña: ", font=("Rockwell Nova Bold", 15), fg= '#2b0d48',  bg='#ADAFE1') 
+    entrada_correo = Entry(ventana_inicio)
+    entrada_contrasenia1 = Entry(ventana_inicio, show="*")
+    label_correo.pack(fill = tkinter.BOTH, pady = 3)
+    entrada_correo.pack(fill = tkinter.BOTH, pady = 10, padx= 70, ipady= 5)
+    label_contrasenia1.pack(fill = tkinter.BOTH, pady = 3)
+    entrada_contrasenia1.pack(fill = tkinter.BOTH, pady = 10, padx= 70, ipady= 5)
+    button_aceptar = Button(ventana_inicio, text="Aceptar", command= lambda: ComprobarConstraseña(entrada_correo.get(), entrada_contrasenia1.get()), font=("Segoe UI", 10))
+    button_volver = Button(ventana_inicio, text="Volver", command= ventana_inicio.destroy, font=("Segoe UI", 10))
+    button_aceptar.pack(pady= 10, padx= 150)
+    button_volver.pack(pady = 10)
+    ventana_inicio.mainloop()
+    
+    return #usuario["Nombre"], usuario["Correo"]
 
 
 ventana = tkinter.Tk()
 ventana.title("Hailo")
-ventana.geometry("550x650")
+ventana.geometry("550x650+50+50")
 ventana.resizable(False, False)
 ventana.config(bg='#ADAFE1')
 bienvenido = tkinter.Label(ventana, text = "Bienvenido a Hailo", font=("Rockwell Nova Extra Bold", 25), fg= '#2b0d48')  
 eslogan = tkinter.Label(ventana, text = "¿Viajamos juntos?", font=("Rockwell Nova Extra Bold", 25), fg= '#2b0d48',  bg='#ADAFE1')  
 img = Image.open( os.path.dirname(__file__) +"/logo.png")
-img = img.resize((200, 200), Image.ANTIALIAS)
+img = img.resize((200, 200), Image.LANCZOS)
 img = ImageTk.PhotoImage(img)
 label = tkinter.Label(ventana, image=img, bg='#ADAFE1')
 bienvenido.pack(fill = tkinter.X, pady = 20)
@@ -180,7 +251,7 @@ button_registro.pack(pady = 10)
 ventana.protocol("WM_DELETE_WINDOW", ventana.destroy) 
 ventana.mainloop()
 
-i = 0                                                         #Al abrir la aplicación puedes registrarte o iniciar sesión
+''' i = 0                                                         #Al abrir la aplicación puedes registrarte o iniciar sesión
 while True:                                             #Si ya tienes una cuenta, puedes iniciar sesión
     if i==0:                                            #Si no tienes una cuenta, puedes registrarte                                                      
         a = input("¿Tienes ya una cuenta? (S/N) ").lower()
@@ -218,5 +289,5 @@ try:
 except FileNotFoundError:
     #sino, solo puede reservar
     print("¡Reserva tu primer viaje!")
-    reservas.reservar(correo_usuario, nombre)
+    reservas.reservar(correo_usuario, nombre) '''
 
